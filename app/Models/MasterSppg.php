3bb12e26d02930a790afca8d1432f325be6a7b10
rifjan29 <rjndla@gmail.com>
2026-03-19 03:24:02 +0700

    test

--- app/Models/MasterSppg.php
+++ app/Models/MasterSppg.php
@@ -2,9 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class MasterSppg extends Model
 {
@@ -14,6 +13,8 @@ class MasterSppg extends Model
         'name',
         'instagram',
         'tiktok',
+        'alamat',
+        'status',
     ];
 
     public function users()
